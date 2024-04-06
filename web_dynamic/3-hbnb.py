fbdc7a44ef1668433de2e213ccbbc283aaf5cafc
y4h14 <yahyasadig2013@gmail.com>
2024-04-06 04:13:13 +0300

    Added task4 loading places through API

--- web_dynamic/3-hbnb.py
+++ web_dynamic/3-hbnb.py
@@ -66,6 +66,29 @@ def hbnb2():
                            cache_id=cache_id)
 
 
+@app.route('/3-hbnb/', strict_slashes=False)
+def hbnb3():
+    """ HBNB is alive! """
+    states = storage.all(State).values()
+    states = sorted(states, key=lambda k: k.name)
+    st_ct = []
+
+    for state in states:
+        st_ct.append([state, sorted(state.cities, key=lambda k: k.name)])
+
+    amenities = storage.all(Amenity).values()
+    amenities = sorted(amenities, key=lambda k: k.name)
+
+    places = storage.all(Place).values()
+    places = sorted(places, key=lambda k: k.name)
+
+    return render_template('3-hbnb.html',
+                           states=st_ct,
+                           amenities=amenities,
+                           places=places,
+                           cache_id=cache_id)
+
+
 if __name__ == "__main__":
     """ Main Function """
     app.run(host='0.0.0.0', port=5000)
--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,4 +1,21 @@
 $(document).ready(function() {
+
+	// task2 - 1-hbnb
+	let amenities = {}
+	$("input:checkbox").each(function(){
+		$(this).on("change", function(event){
+			if ($(this).is(':checked')){
+				amenities[$(this).data('id')] = $(this).data('name');
+			}
+			else {
+				delete amenities[$(this).data('id')];
+			}
+			let amenitiesList = Object.values(amenities).join(', ');
+			$("div.amenities h4").text(amenitiesList);
+		});
+	});
+
+	// task3 - 2-hbnb
 	$.get('http://localhost:5001/api/v1/status/', function (data) {
 		if (data.status === 'OK') {
 			$('#api_status').addClass('available')
