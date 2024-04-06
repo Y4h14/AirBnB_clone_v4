41c36efcd7ccfb3abacc21f7697b7f81807c8eba
Armolas <armolas06@gmail.com>
2024-04-06 15:42:56 +0100

    fixed semistandard errors

--- web_dynamic/static/scripts/3-hbnb.js
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -1,65 +1,60 @@
-$(document).ready(function() {
-	// task2 - 1-hbnb
-	let amenities = {}
-	$("input:checkbox").each(function(){
-		$(this).on("change", function(event){
-			if ($(this).is(':checked')){
-				amenities[$(this).data('id')] = $(this).data('name');
-			}
-			else {
-				delete amenities[$(this).data('id')];
-			}
-			let amenitiesList = Object.values(amenities).join(', ');
-			$("div.amenities h4").text(amenitiesList);
-		});
-	});
+$(document).ready(function () {
+  // task2 - 1-hbnb
+  const amenities = {};
+  $('input:checkbox').each(function () {
+    $(this).on('change', function (event) {
+      if ($(this).is(':checked')) {
+        amenities[$(this).data('id')] = $(this).data('name');
+      } else {
+        delete amenities[$(this).data('id')];
+      }
+      const amenitiesList = Object.values(amenities).join(', ');
+      $('div.amenities h4').text(amenitiesList);
+    });
+  });
 
-	// task3 - 2-hbnb
-	$.get('http://localhost:5001/api/v1/status/', function (data) {
-		if (data.status === 'OK') {
-			$('#api_status').addClass('available')
-		}
-		else {
-			$('#api_status').removeClass('available')
-		}
-	});
+  // task3 - 2-hbnb
+  $.get('http://localhost:5001/api/v1/status/', function (data) {
+    if (data.status === 'OK') {
+      $('#api_status').addClass('available');
+    } else {
+      $('#api_status').removeClass('available');
+    }
+  });
 
-	//task4 - 3-hbnb
-	function renderPlace(place){
-		return `<article>
-		<div class="title_box">
-		  <h2>${ place.name }</h2>
-		  <div class="price_by_night">${place.price_by_night}</div>
-		</div>
-		<div class="information">
-		  <div class="max_guest">${ place.max_guest }  Guest${
-			place.max_guest !== 1 ? "s": ""
-		}</div>
-			  <div class="number_rooms">${ place.number_rooms } Bedroom${
-				place.number_rooms !== 1 ? "s": ""
-			}</div>
-			  <div class="number_bathrooms">${ place.number_bathrooms } Bathroom${
-				place.number_bathrooms !== 1 ? "s" : ""
-			}</div>
-		</div>
-			<div class="description">
-		  ${place.description}
-			</div>
-	  </article>`;
-	}
-	
-	function renderPlaces(places){
-		return places.map(place=> renderPlace(place)).join('');
-	}
-	
-	$.post({
-		url: "http://localhost:5001/api/v1/places_search/",
-		data: JSON.stringify({}),
-		headers: { "content-type": "application/json"}
-	}, function (data) {
-		$("section.places").append(renderPlaces(data));
-	},
-	 dataType="json"
-	);
-	
+  // task4 - 3-hbnb
+  function renderPlace (place) {
+    return `
+    <article>
+    <div class="title_box">
+        <h2>${place.name}</h2>
+      <div class="price_by_night">${place.price_by_night}</div>
+        </div>
+    <div class="information">
+    <div class="max_guest">${place.max_guest}  Guest${
+        place.max_guest !== 1 ? 's' : ''
+        }</div>
+    <div class="number_rooms">${place.number_rooms} Bedroom${
+        place.number_rooms !== 1 ? 's' : ''
+        }</div>
+    <div class="number_bathrooms">${place.number_bathrooms} Bathroom${
+        place.number_bathrooms !== 1 ? 's' : ''
+        }</div>
+    </div>
+    <div class="description">${place.description}</div>
+    </article>`;
+  }
+
+  function renderPlaces (places) {
+    return places.map(place => renderPlace(place)).join('');
+  }
+
+  $.post({
+    url: 'http://localhost:5001/api/v1/places_search/',
+    data: JSON.stringify({}),
+    headers: { 'content-type': 'application/json' },
+    dataType: 'json'
+  }, function (data) {
+    $('section.places').append(renderPlaces(data));
+  });
 });
